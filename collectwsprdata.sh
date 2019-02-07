2a016fd14219ffa6da89d66f02b30fa467f58ea1
Tom Bingham <tominthetrees@gmail.com>
2019-02-06 16:59:59 -0800

    Added wsprstats database file, modified collectwsprdata conf and sh files to use sqlite.

--- collectwsprdata.sh
+++ collectwsprdata.sh
@@ -4,34 +4,28 @@
 
 #This script downloads the monthly wspr data file, uses awk and gz
 #to collect information on a specific callsign, and inserts the resulting
-#data into a mysql database table.
+#data into a sqlite database table.
 
-cd ${HOME}/bin
+#cd ${HOME}/tom_bin
+cd /cygdrive/c/bingroot/Development/WSPR-stats/
 . collectwsprdata.conf
-
-echo $0
-echo $#
 
 if [ $# -eq 1 ]
 then
-  wspr_file_name=$wspr_file_base_name_beginning$1$wspr_file_base_name_end
+  wspr_filename=$wspr_file_base_name_beginning$1$wspr_file_base_name_end
 else
-  #wspr_file_name=$wspr_file_base_name_beginning$(date --date="1 month ago" +%Y-%m)$wspr_file_base_name_end
-  wspr_file_name=$wspr_file_base_name_beginning$(date +%Y-%m)$wspr_file_base_name_end
+  wspr_filename=$wspr_file_base_name_beginning$(date +%Y-%m)$wspr_file_base_name_end
 fi
 
-echo $wspr_file_name
-#exit
+echo $wspr_filename
 
-#Testing download file name
-#wspr_file_name=wb7eux.csv.gz
+wget $wspr_file_location$wspr_filename
 
-wget $wspr_file_location$wspr_file_name
+awk '/M0RNR/' <(gzip -dc $wspr_filename) > $output_filename
 
-#awk '/WB7EUX/' <(gzip -dc $wspr_file_name) > $output_file_name
-awk '/$filter_phrase/' <(gzip -dc $wspr_file_name) > $output_file_name
+sqlite3 -batch "${db_file}" <<EOF
+.separator "${field_terminater}"
+.import "${output_filename}" "${load_table}"
+EOF
 
-mysqlimport -u $user -p$password -h $server --fields-terminated-by=',' -S'/opt/lampp/var/mysql/mysql.sock' $database $output_file_name
-
-#rm $output_file_name $wspr_file_name
-
+rm $output_filename $wspr_filename
