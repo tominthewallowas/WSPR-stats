5adb2098a0c1a869dec4ef1203215e889f6359e7
Tom Bingham <tominthetrees@gmail.com>
2019-02-21 17:10:34 -0800

    Still a shell - added a menu item to exit app.

--- UI_wsprstats.py
+++ UI_wsprstats.py
@@ -11,17 +11,23 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(800, 600)
+        MainWindow.resize(595, 600)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 21))
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 595, 21))
         self.menubar.setObjectName("menubar")
+        self.menu_Quit = QtWidgets.QMenu(self.menubar)
+        self.menu_Quit.setObjectName("menu_Quit")
         MainWindow.setMenuBar(self.menubar)
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
         self.statusbar.setObjectName("statusbar")
         MainWindow.setStatusBar(self.statusbar)
+        self.action_Quit = QtWidgets.QAction(MainWindow)
+        self.action_Quit.setObjectName("action_Quit")
+        self.menu_Quit.addAction(self.action_Quit)
+        self.menubar.addAction(self.menu_Quit.menuAction())
 
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
@@ -29,4 +35,6 @@ class Ui_MainWindow(object):
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.menu_Quit.setTitle(_translate("MainWindow", "&File"))
+        self.action_Quit.setText(_translate("MainWindow", "&Quit"))
 
