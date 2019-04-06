031ca4af64a5eeb6958f9fe3a0c0898e81ee0cf5
Tom Bingham <tominthetrees@gmail.com>
2019-04-06 21:30:21 +0100

    Moved statements into main file.

--- sqlstatements.py
+++
@@ -1,5 +0,0 @@
-# SQL statements
-
-xmit_callsigns = "select distinct xmit_callsign, xmit_grid from wspr_stats order by xmit_callsign"
-
-reporter_callsigns = "select distinct reporter, reporter_grid from wspr_stats order by reporter"
