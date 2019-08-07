06002991bdd9a6bd39d759ee1812bfb4bf6c69af
tombo <tominthetrees@gmail.com>
2019-08-07 12:46:27 -0700

    Just catching up after a long time.

--- wsprstats.py
+++ wsprstats.py
@@ -16,17 +16,19 @@ from PySide2.QtCore import (Qt, QDate, QDateTime)
 from PySide2.QtSql import (QSqlQuery, QSqlQueryModel)
 from UI_wsprstats import *
 
-import tombo.configfile
+from configparser import ConfigParser
 import sqlitedatabase
 import sqlcasephrases
 from selectionchoices import SelectionChoices
+
 
 class WSPRStats(QMainWindow):
     def __init__(self):
         super().__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
-        self.configuration = tombo.configfile.ConfigFile('wsprstats.conf').getItems('CONFIG')
+        #self.configuration = tombo.configfile.ConfigFile('wsprstats.conf').getItems('CONFIG')
+        self.configuration = self.gatherConfiguration('wsprstats.conf')
         self.db = sqlitedatabase.SqliteDatabase(self.configuration['db_file'])
         self.ui.lwConfigInfo.addItem('Datebase file: ' + self.configuration['db_file'])
         self.ui.lwConfigInfo.addItem('Tracked Callsign: ' + self.configuration['trackedcallsign'])
@@ -35,7 +37,12 @@ class WSPRStats(QMainWindow):
         self.setupMethods()
         self.show()
 
-    #----------------------------------------------------------------------
+    def gatherConfiguration(self, filename):
+        self.configparser = ConfigParser()
+        self.configparser.read(filename)
+        return dict(self.configparser.items('CONFIG'))
+
+    # ----------------------------------------------------------------------
     def initialize(self):
         self.setDateTimeEdits()
         self.dateButtonGroup = QButtonGroup()
@@ -44,7 +51,7 @@ class WSPRStats(QMainWindow):
         self.loadCategoryButtonGroup()
         self.setDateGroupBoxStatus(self.RADIOBUTTON_ALL_DATES)
 
-    #----------------------------------------------------------------------
+    # ----------------------------------------------------------------------
     def setupMethods(self):
         self.ui.action_Quit.triggered.connect(self.close)
         self.ui.pbQuit.clicked.connect(self.close)
@@ -52,7 +59,7 @@ class WSPRStats(QMainWindow):
         self.dateButtonGroup.buttonClicked[int].connect(self.setDateGroupBoxStatus)
         self.categoryButtonGroup.buttonClicked[int].connect(self.setCategoryChoice)
 
-    #----------------------------------------------------------------------
+    # ----------------------------------------------------------------------
     def loadCategoryButtonGroup(self):
         self.RADIOBUTTON_NO_CATEGORY = 0
         self.RADIOBUTTON_BY_REPORTER_TRANSMITTER_BAND = 1
@@ -69,7 +76,7 @@ class WSPRStats(QMainWindow):
                 self.categoryButtonGroup.addButton(button, self.RADIOBUTTON_BY_REPORTING_STATION)
             elif button.objectName() == 'rbByTransmittingStation':
                 self.categoryButtonGroup.addButton(button, self.RADIOBUTTON_BY_TRANSMITTING_STATION)
-    #----------------------------------------------------------------------
+    # ----------------------------------------------------------------------
 
     def loadDateButtonGroup(self):
         self.RADIOBUTTON_ALL_DATES = 0
@@ -90,40 +97,41 @@ class WSPRStats(QMainWindow):
             elif button.objectName() == 'rbLast30Days':
                 self.dateButtonGroup.addButton(button, self.RADIOBUTTON_LAST_30_DAYS)
 
-    #----------------------------------------------------------------------
+    # ----------------------------------------------------------------------
     def setDateTimeEdits(self):
         self.ui.dteFrom.setDateTime(QDateTime.currentDateTime().toUTC())
         self.ui.dteTo.setDateTime(QDateTime.currentDateTime().toUTC())
 
-    #----------------------------------------------------------------------
+    # ----------------------------------------------------------------------
     def setCategoryChoice(self, buttonId):
         self.selectionchoices.setCategoryChoice(buttonId)
 
-    #----------------------------------------------------------------------
+    # ----------------------------------------------------------------------
     def setDateGroupBoxStatus(self, buttonId):
         self.selectionchoices.setDateChoice(buttonId)
         self.ui.gbDateRange.setEnabled(True if buttonId == self.RADIOBUTTON_DATERANGE else False)
 
-    #----------------------------------------------------------------------
+    # ----------------------------------------------------------------------
     def selectModel(self):
         conn = self.db.getConnection()
         model = QSqlQueryModel()
         query = QSqlQuery(conn)
         self.buildSqlStatement(query)
         query.exec_()
-        if query.lastError().driverText(): print("Query error msg:", query.lastError().driverText())
+        if query.lastError().driverText():
+            print("Query error msg:", query.lastError().driverText())
         model.setQuery(query)
         self.ui.tblStats.setModel(model)
         self.ui.tblStats.horizontalHeader().setSectionResizeMode(QHeaderView.ResizeToContents)
 
-    #----------------------------------------------------------------------
+    # ----------------------------------------------------------------------
     def buildSqlStatement(self, query):
         callsign = self.configuration['trackedcallsign']
-        
+
         if self.selectionchoices.getDateChoice() == self.RADIOBUTTON_ALL_DATES:
             if self.selectionchoices.getCategoryChoice() == self.RADIOBUTTON_NO_CATEGORY:
                 sql = "select reporter as Reporter, reporter_grid as 'Rpt Grid', xmit_callsign as 'Transmitter', xmit_grid as 'Xmit Grid', " + sqlcasephrases.sql_power_case_phrase + " as Power, " + sqlcasephrases.sql_band_case_phrase + " as Band, distance as 'Distance(km)', datetime(timestamp, 'unixepoch') as 'Date/Time', signal_noise_ratio as SNR from wspr_stats where (reporter = :callsign or xmit_callsign = :callsign) order by reporter, xmit_callsign"
-        
+
             if self.selectionchoices.getCategoryChoice() == self.RADIOBUTTON_BY_REPORTER_TRANSMITTER_BAND:
                 sql = "select reporter as Reporter, xmit_callsign as 'Transmitter', " + sqlcasephrases.sql_band_case_phrase + " as Band, count(*) as 'Band Count' from wspr_stats where reporter = :callsign or xmit_callsign = :callsign group by reporter, xmit_callsign, band order by reporter, xmit_callsign, band"
 
@@ -156,6 +164,7 @@ class WSPRStats(QMainWindow):
             query.bindValue(':callsign', callsign)
             query.bindValue(':from', int(fromDateTime))
             query.bindValue(':to', int(toDateTime))
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
